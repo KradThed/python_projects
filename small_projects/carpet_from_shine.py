9567109ebdb00504067bf94c1c4f10739d619209
Asura <71103744+KradThed@users.noreply.github.com>
2024-08-11 08:25:10 +0300

    Carpet

---
+++ small_projects/carpet_from_shine.py
@@ -0,0 +1,13 @@
+
+
+X_REPEAT = 6
+Y_REPEAT = 4
+
+for i in range(Y_REPEAT):
+    print(r'_ \ \ \_/ __' * X_REPEAT)
+    print(r' \ \ \___/ _' * X_REPEAT)
+    print(r'\ \ \_____/ ' * X_REPEAT)
+    print(r'/ / / ___ \_' * X_REPEAT)
+    print(r'_/ / / _ \__' * X_REPEAT)
+    print(r'__/ / / \___' * X_REPEAT)
+
